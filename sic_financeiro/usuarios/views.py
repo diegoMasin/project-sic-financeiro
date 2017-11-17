caea270a9533dd34aba3b8b4bd0ea93b85232730
Saulo Viana <sauloviana483@gmail.com>
2017-11-17 15:52:10 -0300

    Item_2 Concluído

--- sic_financeiro/usuarios/views.py
+++ sic_financeiro/usuarios/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render, redirect
 from sic_financeiro.usuarios.forms import UserModelForm
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 from sic_financeiro.core.views.context_urls import context_urls as context_global
 
 
@@ -12,14 +13,16 @@ def signup(request):
     if request.method == 'POST':
         if form.is_valid():
             try:
-                check_termo = request.POST['check_termo']
-                # form.save()
+                request.POST['check_termo']
+                form.save()
                 messages.success(request, 'Usuário cadastrado com sucesso!')
 
                 return redirect('/login')
-
             except:
                 messages.warning(request, 'Marque se concorda com os Termos')
+        else:
+            if form.errors['username']:
+                messages.warning(request, form.errors['username'][0])
 
     context_global.context['form'] = form
     return render(request, '{0}/signup.html'.format(context_global.path_login), context_global.context)
