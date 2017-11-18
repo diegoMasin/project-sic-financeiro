14525012f169fa8665e4a830a025ea1ca055bd98
Saulo Viana <sauloviana483@gmail.com>
2017-11-17 23:41:11 -0300

    Item_3 Concluído

--- sic_financeiro/static/v1/especifico/auth/main.js
+++ sic_financeiro/static/v1/especifico/auth/main.js
@@ -1,14 +1,38 @@
 /** SIGNUP **/
+function showMessageNotify(text) {
+    //$('.gerado-mensagem').attr('onclick', '2');
+    $('.gerado-mensagem').attr('onclick', '$.Notification.autoHideNotify(\'warning\', \'top right\', \'Mensagem:\',\''+text+'\')');
+    $('.gerado-mensagem').click();
+}
+
+function validPasswordsEqual(inputs) {
+    if ($(inputs[1]).val() !== $(inputs[0]).val() || (!$.trim($(inputs[1]).val()) && !$.trim($(inputs[0]).val()))) {
+        showMessageNotify('A senha não corresponde! Por favor, repita a senha corretamente.');
+        $(inputs).parents('.input-group').addClass('has-warning');
+        $(inputs).val('');
+        $(inputs[0]).focus();
+        return false;
+    } else {
+        if ($(inputs).parents('.input-group').hasClass('has-warning')) {
+            $(inputs).parents('.input-group').removeClass('has-warning');
+        }
+        return true;
+    }
+}
+
 $(document).ready(function(){
 
     var inputs = $('form').find('input[type="password"]');
 
     if (inputs !== 'undefined') {
         $(inputs[1]).focusout(function () {
-            console.log('Saiu do input!');
-            if ($(this).val() !== $(inputs[0]).val())
-            {
-                console.log('Senha não corresponde! Por favor, redigite a senha.');
+            validPasswordsEqual(inputs);
+        });
+
+        $('form').submit(function(e) {
+            if (!validPasswordsEqual(inputs)) {
+                e.preventDefault();
+                return false;
             }
         });
     }
