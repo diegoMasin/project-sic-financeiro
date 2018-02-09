5a10d19f79e90c09e76ace152d2277bd95675c6e
Diego Masin <diego.masin@defensoria.ce.def.br>
2018-02-09 13:35:57 -0300

    Teste de compressão do Whitenoise.

--- sic_financeiro/settings.py
+++ sic_financeiro/settings.py
@@ -138,7 +138,7 @@ STATIC_ROOT = os.path.join(DATA_DIR, 'static')
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'sic_financeiro', 'static'),
 )
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 FILE_UPLOAD_HANDLERS = (
     'django.core.files.uploadhandler.TemporaryFileUploadHandler',
